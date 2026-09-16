5ede4e6bd255db436d8a5cdd708b68900439c7ed
raiakmal <muhammadrai0504@gmail.com>
2026-09-16 16:28:17 +0700

    fix: use laravel mix production build

--- database/katalis.sql
+++ database/katalis.sql
@@ -15,9 +15,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Membuang struktur basisdata untuk katalisbptph_dummy
-CREATE DATABASE IF NOT EXISTS `katalisbptph_dummy` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `katalisbptph_dummy`;
+USE `railway`;
 
 -- membuang struktur untuk table katalisbptph_dummy.analyst_tasks
 CREATE TABLE IF NOT EXISTS `analyst_tasks` (
